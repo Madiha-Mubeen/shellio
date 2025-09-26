315940d2f9f2eba86a70646599b82c7968155369
Madiha <madihamubeenhawaldar@gmail.com>
2025-09-26 19:21:10 +0530

    adding navbar

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav>
+            <h1>Explore The - Shellio</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
