14f7ca37cacae9692c65e4bd0bcf25b96012660b
Madiha <madihamubeenhawaldar@gmail.com>
2025-09-29 22:30:00 +0530

    about added

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+    return (
+    <div>
+        <h1>Explore the - Shellio About</h1>
+        <p> In this website you will find or scroll videos about shell based, you can explore on your own & experience the DIY, your journey through it. </p>
+    </div>
+    );
+}
+
+export default About;
