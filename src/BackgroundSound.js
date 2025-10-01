74bbdb4a2ef624b5e7e11f79099f7197e35767ac
Madiha <madihamubeenhawaldar@gmail.com>
2025-10-01 20:42:57 +0530

    deebugging

---
+++ src/BackgroundSound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function BackgroundSound() {
+    return (
+    <div className="audio">
+        <audio loop muted controls autoPlay>
+            <source src={process.env.PUBLIC_URL + "./calm-nature-sounds.mp3"} type="audio/mpeg" /> 
+            Oh looks like your browsers does not support the audio tag 
+        </audio>
+    </div>
+    )
+}
+
+export default BackgroundSound;
