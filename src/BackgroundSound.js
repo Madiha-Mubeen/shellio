2534e6d8020dbcab7115178307d93cdfdb65ddec
Madiha <madihamubeenhawaldar@gmail.com>
2025-10-01 20:46:52 +0530

    debugged

--- src/BackgroundSound.js
+++ src/BackgroundSound.js
@@ -4,7 +4,7 @@ function BackgroundSound() {
     return (
     <div className="audio">
         <audio loop muted controls autoPlay>
-            <source src={process.env.PUBLIC_URL + "/calm-nature-sounds.mp3"} type="audio/mpeg" /> 
+            <source src={process.env.PUBLIC_URL + "./calm-nature-sounds.mp3"} type="audio/mpeg" /> 
             Oh looks like your browsers does not support the audio tag 
         </audio>
     </div>
