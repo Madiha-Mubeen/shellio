56a9049bab69607d1b01c66eb8ef33e6db50d0ad
Madiha <madihamubeenhawaldar@gmail.com>
2025-10-01 22:25:29 +0530

    debugged typo for videos path

--- src/Main.js
+++ src/Main.js
@@ -5,67 +5,67 @@ function Main() {
 
     const videosList = [
         {
-            src: process.env.PUBLIC_URL + "/videos/shellMirror.mp4",
+            src: process.env.PUBLIC_URL + "/Videos/mirrorshell.mp4",
             title: "shelly Mirror",
             description: "Aesthetic & Beautiful"
 
         },
         {
-            src: process.env.PUBLIC_URL + "/videos/cowriebracelet.mp4",
+            src: process.env.PUBLIC_URL + "/Videos/braceletcowrie.mp4",
             title: "Cowrie Bracelet",
             description: "Looks so beautiful"
         },
         {
-            src: process.env.PUBLIC_URL + "/videos/shellMakeupkit.mp4",
+            src: process.env.PUBLIC_URL + "/Videos/makeupkitshell.mp4",
             title: "Beautiful makeup kit",
             description: "Barbie style Makeupkit"
         },
         {
-            src: process.env.PUBLIC_URL + "/videos/shellStand.mp4",
+            src: process.env.PUBLIC_URL + "/Videos/standshell.mp4",
             title: "Elegant shiny shell stand",
             description: "Graceful look"
         },
         {
-            src: process.env.PUBLIC_URL + "/videos/shellCanvas.mp4",
+            src: process.env.PUBLIC_URL + "/Videos/canvasshell.mp4",
             title: "good looking canvas",
             description: "Can be done during free time when feel'in to draw on canvas with some different style."
         },
         {
-            src: process.env.PUBLIC_URL + "/videos/shellpaintPalette.mp4",
+            src: process.env.PUBLIC_URL + "/Videos/shellpaint.mp4",
             title: "Shell Paint Palette",
             description: "You can use this in your next painting time !"
         },
         {
-            src: process.env.PUBLIC_URL + "/videos/shellCandle.mp4",
+            src: process.env.PUBLIC_URL + "/Videos/candleshell.mp4",
             title: "Shell Based Candles",
             description: "Decor your Home with these"
         },
         {
-            src: process.env.PUBLIC_URL + "/videos/shellNecklace.mp4",
+            src: process.env.PUBLIC_URL + "/Videos/necklaceshell.mp4",
             title: "Pretty Shell Necklace",
             description: "Will suits you well."
         },
         {
-            src: process.env.PUBLIC_URL + "/videos/shellclaycraft.mp4",
+            src: process.env.PUBLIC_URL + "/Videos/claycraftshell.mp4",
             title: "Shell Clay Craft",
             description: "Fits with your DIY Collection"
         }, {
-            src: process.env.PUBLIC_URL + "/videos/shellAntique.mp4",
+            src: process.env.PUBLIC_URL + "/Videos/antiqueshell.mp4",
             title: "Easy DIY",
             description: "This looks good in your Sider"
         },
         {
-            src: process.env.PUBLIC_URL + "/videos/shellBowl.mp4",
+            src: process.env.PUBLIC_URL + "/Videos/bowlshell.mp4",
             title: "Sell Bowl",
             description: "Easy to make & ready to use"
         },
         {
-            src: process.env.PUBLIC_URL + "/videos/shellMermaidmirror.mp4",
+            src: process.env.PUBLIC_URL + "/Videos/shellmermaidmir.mp4",
             title: "Mermaid Mirror",
             description: "It fits so well the your wardrobe table"
         },
         {
-            src: process.env.PUBLIC_URL + "/videos/shellphoneCharm.mp4",
+            src: process.env.PUBLIC_URL + "/Videos/shellphonech.mp4",
             title: "Crystal Shell Phone Charm",
             description: "Looks good on the phone <3"
         }
