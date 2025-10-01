1de18eccc1304c16df433203ec92857415fae300
Madiha <madihamubeenhawaldar@gmail.com>
2025-10-01 20:10:06 +0530

    debugging audio

--- src/BackgroundSound.js
+++ src/BackgroundSound.js
@@ -3,7 +3,10 @@ import React from "react";
 function BackgroundSound() {
     return (
     <div className="audio">
-        <audio src={process.env.PUBLIC_URL + "/calm-nature-sounds.mp3"} loop controls autoPlay  />
+        <audio loop muted controls autoPlay>
+            <source src={process.env.PUBLIC_URL + "/calm-nature-sounds.mp3"} type="audio/mpeg" /> 
+            Oh looks like your browsers does not support the audio tag 
+        </audio>
     </div>
     )
 }
